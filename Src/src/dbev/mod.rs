b36fe02e331e26abcac186ca90b47c377bad1cc8
Tomáš Labanc <166916054+Tomaxikz@users.noreply.github.com>
2026-08-14 19:01:39 +0200

    YAA

--- Src/src/dbev/mod.rs
+++
@@ -1,5 +0,0 @@
-mod api;
-mod client;
-
-pub use api::{DbevCapabilities, validate_system_response};
-pub use client::DbevClient;
--- Src/src/domain/mod.rs
+++
@@ -1,5 +0,0 @@
-mod protocol;
-mod server_configuration;
-
-pub use protocol::{DatabaseProtocol, ResourceLimits};
-pub use server_configuration::{ServerDatabaseConfigurationExtension, register_server_integration};
